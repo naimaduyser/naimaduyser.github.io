c214deed512944c95e3abebc20cb00ab6000fd4a
Naima Duyser <naima.fatimaduyser@gmail.com>
2022-07-04 20:25:33 +0200

    updated bio

--- src/routes/About.js
+++ src/routes/About.js
@@ -16,23 +16,24 @@ const About = () => {
           <p>
             In 2018 I graduated from DBS Film School (now known as Catalyst:
             Institute of the Arts) in Berlin. Having been to film school
-            solidified my undying love for cinema and visual arts. I love
-            experimenting in different fields of filmmaking so my work covers a
-            diverse range of subjects, styles and genres: music videos, short
-            films, commercials and documentaries. My main strengths are in
+            solidified my infinite love for cinema and visual arts. I experimented in different fields of filmmaking so my work covers a
+            diverse range of subjects, styles and genres. My main strengths are in
             directing, producing and editing.
           </p>
           <p>
             I am also a recent graduate of Le Wagon's 9 week intensive web
             development bootcamp, where I learned the foundations of web
             development, including HTML, CSS & JavaScript, Bootstrap, jQuery,
-            SQL, Git & Version Control, Heroku, Ruby on Rails, AJAX and
-            UX/UI Design. At the moment I am doing an internship as a front-end
-            web developer at AKQA Amsterdam. During this time I am teaching myself how to use different JavaScript frameworks such as Vue.js and React.
+            SQL, Git & Version Control, Heroku, Ruby on Rails, AJAX and UX/UI
+            Design. Since March 2022, I started an internship in web
+            development at AKQA Amsterdam. During my time here, I familiarised
+            myself with different JavaScript frameworks, such as React and
+            Vue.js.
           </p>
           <p>
             Feel free to reach out to me about any projects you may need help
-            with. You can see examples of my work by clicking on the links above. Happy Browsing!
+            with. You can see examples of my work by clicking on the links
+            above. Happy Browsing!
           </p>
         </div>
       </div>
